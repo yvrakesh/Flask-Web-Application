b1bee18a53a051f8c38240fdf059010c75e05066
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2021-06-23 22:08:59 +0530

    Added Register, Login, Logout, Auth requirement to access a page and some project restructuring

--- Market/__init__.py
+++ Market/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 # Initialized Instance of Flask 
 # Built in variable that is referring to the local python file that we're working with
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '212f66253f812a24285b0045'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from Market import routes
 
 
--- Market/routes.py
+++ Market/routes.py
@@ -1,6 +1,11 @@
+from Market.forms import LoginForm, RegisterForm
 from Market import app
-from flask import render_template
-from Market.models import Item
+from flask import render_template,redirect,url_for, flash, get_flashed_messages
+from Market.models import Item, User
+from Market.forms import RegisterForm
+from Market.models import db
+from flask_login import login_user,logout_user, login_required
+
 
 @app.route('/')
 @app.route('/home')
@@ -8,17 +13,42 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html',items = items)
 
-# @app.route('/')
-# def hello_world():
-#     return '<h1>Hello World</h1>'
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db.session.add(User(username=form.username.data, email_address=form.email.data, password=form.password1.data))
+        db.session.commit()
+        flash(f'Success! Your account with username "{form.username.data}" is successfully created',category='success')
+        return redirect(url_for('home_page'))
+    if form.errors != {}: # If there are no errors for validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating an user:{err_msg}',category='danger')
+    return render_template('register.html',form=form)
 
-# @app.route('/about')
-# def about_page():
-#     return '<h1>You are in routes page</h1>'
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}',category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash("Username and Password don't match! Please try Again",category='danger')
+    return render_template('login.html',form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been successfully logged out',category='info')
+    return redirect(url_for("home_page"))
 
 # @app.route('/profile/<username>')
 # def user_profile(username):
