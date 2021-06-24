7408ee2291b85f6124b9b8db2ae47057adff6766
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2021-06-24 16:08:14 +0530

    Added Purchasing and Selling Products and list showing currently available products to buy or sell

--- Market/models.py
+++ Market/models.py
@@ -29,6 +29,12 @@ class User(db.Model,UserMixin):
     
     def check_password_correction(self,attempted_password):
         return bcrypt.check_password_hash(self.password_hash,attempted_password)
+    
+    def can_purchase(self,item_obj):
+        return self.budget >= item_obj.price
+    
+    def can_sell(self,item_obj):
+        return item_obj in self.items
 
 class Item(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
@@ -40,3 +46,4 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+    
--- Market/routes.py
+++ Market/routes.py
@@ -1,10 +1,10 @@
-from Market.forms import LoginForm, RegisterForm
+from Market.forms import LoginForm, RegisterForm, PurchaseItemForm, SellItemForm
 from Market import app
-from flask import render_template,redirect,url_for, flash, get_flashed_messages
+from flask import render_template,redirect,url_for, flash, get_flashed_messages, request
 from Market.models import Item, User
 from Market.forms import RegisterForm
 from Market.models import db
-from flask_login import login_user,logout_user, login_required
+from flask_login import login_user,logout_user, login_required, current_user
 
 
 @app.route('/')
@@ -12,11 +12,40 @@ from flask_login import login_user,logout_user, login_required
 def home_page():
     return render_template('home.html')
 
-@app.route('/market')
+@app.route('/market',methods=['GET','POST'])
 @login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html',items = items)
+    purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
+    if request.method == 'POST':
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.owner = current_user.id
+                current_user.budget -= p_item_object.price
+                db.session.commit()
+                flash(f'Congratulations! You purchased the item {p_item_object.name} for {p_item_object.price}$.',category="success")
+            else:
+                flash(f'Unfortunately you have {p_item_object.price - current_user.budget}$ less credit to purchase {p_item_object.name}',category="danger")
+        
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.owner = None
+                current_user.budget += s_item_object.price
+                db.session.commit()
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
+        return redirect(url_for('market_page'))
+    if request.method == 'GET':
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html',items = items, purchase_form = purchase_form, owned_items=owned_items,selling_form = selling_form)
 
 @app.route('/register',methods=['GET','POST'])
 def register_page():
